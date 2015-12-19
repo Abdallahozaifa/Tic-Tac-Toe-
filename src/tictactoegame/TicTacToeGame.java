559a49a109a92421f878d95c2b901c65b3a0942f
abdallahozaifa <abdallahozaifa19527@gmail.com>
2015-12-19 01:36:09 -0500

    first commit

---
+++ src/tictactoegame/TicTacToeGame.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tictactoegame;
+
+/**
+ *
+ * @author hozaifa
+ */
+public class TicTacToeGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         TicTacToe game = new TicTacToe();
+        game.printHeader();
+        game.printBoard();
+        
+        // plays the tic tac toe game until the game is over
+        while(!game.gameOver()){
+        	// if it is player 1's turn it asks player to enter his/her move
+            if(game.isPlayer1Turn()){
+            	while(!game.isValidMove(game.player1Turn())){
+            		// Will not continue until the user moves in a valid position
+            	}
+	            	game.whatPiece();
+	                game.placeMarker(game.rightMove());
+            }else{
+            	   game.enhancedComputerMove();
+	               game.whatPiece();
+	               game.placeMarker(game.rightMove());
+            }
+            game.printBoard();
+        } 
+    }
+    
+}
